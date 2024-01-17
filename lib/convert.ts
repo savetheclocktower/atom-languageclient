85d77c6c5dab5f7761347e934f9aecadd3c5f92b
Andrew Dupont <github@andrewdupont.net>
2024-01-16 19:49:26 -0800

    Reformatting.

--- lib/convert.ts
+++ lib/convert.ts
@@ -11,10 +11,10 @@ import { diagnosticTypeToLSSeverity, atomIdeDiagnosticToLSDiagnostic } from "./a
  */
 export default class Convert {
   /**
-   * Public: Convert a path to a Uri.
+   * Public: Convert a path to a URI.
    *
-   * @param filePath A file path to convert to a Uri.
-   * @returns The Uri corresponding to the path. e.g. file:///a/b/c.txt
+   * @param filePath A file path to convert to a URI.
+   * @returns The URI corresponding to the path — e.g., `file:///a/b/c.txt`.
    */
   public static pathToUri(filePath: string): string {
     if (new URL(filePath, "file://").protocol !== "file:") {
@@ -28,11 +28,12 @@ export default class Convert {
   }
 
   /**
-   * Public: Convert a Uri to a path.
+   * Public: Convert a URI to a path.
    *
-   * @param uri A Uri to convert to a file path.
-   * @returns A file path corresponding to the Uri. e.g. /a/b/c.txt If the Uri does not begin file: then it is returned
-   *   as-is to allow Atom to deal with http/https sources in the future.
+   * @param uri A URI to convert to a file path.
+   * @returns A file path corresponding to the URI. e.g. /a/b/c.txt If the URI
+   *   does not begin with `file:` then it is returned as-is to allow Atom to
+   *   deal with http/https sources in the future.
    */
   public static uriToPath(uri: string): string {
     const url = new URL(uri, "file://")
@@ -52,40 +53,47 @@ export default class Convert {
   }
 
   /**
-   * Public: Convert an Atom {Point} to a language server {Position}.
+   * Public: Convert an Atom {@link Point} to a language server
+   * {@link Position}.
    *
-   * @param point An Atom {Point} to convert from.
-   * @returns The {Position} representation of the Atom {PointObject}.
+   * @param point An Atom {@link Point} to convert from.
+   * @returns The {@link Position} representation of the Atom
+   *   {@link PointObject}.
    */
   public static pointToPosition(point: Point): ls.Position {
     return { line: point.row, character: point.column }
   }
 
   /**
-   * Public: Convert a language server {Position} into an Atom {PointObject}.
+   * Public: Convert a language server {@link Position} into an Atom
+   * {@link PointObject}.
    *
-   * @param position A language server {Position} to convert from.
-   * @returns The Atom {PointObject} representation of the given {Position}.
+   * @param position A language server {@link Position} to convert from.
+   * @returns The Atom {@link PointObject} representation of the given
+   *   {@link Position}.
    */
   public static positionToPoint(position: ls.Position): Point {
     return new Point(position.line, position.character)
   }
 
   /**
-   * Public: Convert a language server {Range} into an Atom {Range}.
+   * Public: Convert a language server {@link Range} into an Atom {@link Range}.
    *
-   * @param range A language server {Range} to convert from.
-   * @returns The Atom {Range} representation of the given language server {Range}.
+   * @param range A language server {@link Range} to convert from.
+   * @returns The Atom {@link Range} representation of the given language
+   *   server {@link Range}.
    */
   public static lsRangeToAtomRange(range: ls.Range): Range {
     return new Range(Convert.positionToPoint(range.start), Convert.positionToPoint(range.end))
   }
 
   /**
-   * Public: Convert an Atom {Range} into an language server {Range}.
+   * Public: Convert an Atom {@link Range} into an language server
+   * {@link Range}.
    *
-   * @param range An Atom {Range} to convert from.
-   * @returns The language server {Range} representation of the given Atom {Range}.
+   * @param range An Atom {@link Range} to convert from.
+   * @returns The language server {@link Range} representation of the given
+   *   Atom {@link Range}.
    */
   public static atomRangeToLSRange(range: Range): ls.Range {
     return {
@@ -95,23 +103,29 @@ export default class Convert {
   }
 
   /**
-   * Public: Create a {TextDocumentIdentifier} from an Atom {TextEditor}.
+   * Public: Create a {@link TextDocumentIdentifier} from an Atom
+   * {@link TextEditor}.
    *
-   * @param editor A {TextEditor} that will be used to form the uri property.
-   * @returns A {TextDocumentIdentifier} that has a `uri` property with the Uri for the given editor's path.
+   * @param editor A {@link TextEditor} that will be used to form the `uri`
+   *   property.
+   * @returns A {@link TextDocumentIdentifier} that has a `uri` property with
+   *   the URI for the given editor's path.
    */
   public static editorToTextDocumentIdentifier(editor: TextEditor): ls.TextDocumentIdentifier {
     return { uri: Convert.pathToUri(editor.getPath() || "") }
   }
 
   /**
-   * Public: Create a {TextDocumentPositionParams} from a {TextEditor} and optional {Point}.
+   * Public: Create a {@link TextDocumentPositionParams} from a
+   * {@link TextEditor} and optional {@link Point}.
    *
-   * @param editor A {TextEditor} that will be used to form the uri property.
-   * @param point An optional {Point} that will supply the position property. If not specified the current cursor
+   * @param editor A {@link TextEditor} that will be used to form the uri property.
+   * @param point An optional {@link Point} that will supply the position property. If not specified the current cursor
    *   position will be used.
-   * @returns A {TextDocumentPositionParams} that has textDocument property with the editors {TextDocumentIdentifier}
-   *   and a position property with the supplied point (or current cursor position when not specified).
+   * @returns A {@link TextDocumentPositionParams} that has a `textDocument`
+   *   property with the editor's {@link TextDocumentIdentifier} and a
+   *   `position` property with the supplied point (or current cursor position
+   *   when not specified).
    */
   public static editorToTextDocumentPositionParams(editor: TextEditor, point?: Point): ls.TextDocumentPositionParams {
     return {
@@ -121,12 +135,13 @@ export default class Convert {
   }
 
   /**
-   * Public: Create a string of scopes for the atom text editor using the data-grammar selector from an {Array} of
-   * grammarScope strings.
+   * Public: Create a string of scopes for the Atom text editor using the
+   * `data-grammar` selector from an array of `grammarScope` strings.
    *
-   * @param grammarScopes An {Array} of grammar scope string to convert from.
-   * @returns A single comma-separated list of CSS selectors targetting the grammars of Atom text editors. e.g. `['c',
-   *   'cpp']` => `'atom-text-editor[data-grammar='c'], atom-text-editor[data-grammar='cpp']`
+   * @param grammarScopes An array of grammar scope strings to convert from.
+   * @returns A single comma-separated list of CSS selectors targetting the
+   *   grammars of Atom text editors — e.g., `['c', 'cpp']` =>
+   *   `'atom-text-editor[data-grammar='c'], atom-text-editor[data-grammar='cpp']`
    */
   public static grammarScopesToTextEditorScopes(grammarScopes: string[]): string {
     return grammarScopes
@@ -135,11 +150,13 @@ export default class Convert {
   }
 
   /**
-   * Public: Encode a string so that it can be safely used within a HTML attribute - i.e. replacing all quoted values
-   * with their HTML entity encoded versions. e.g. `Hello"` becomes `Hello&quot;`
+   * Public: Encode a string so that it can be safely used within a HTML
+   * attribute - i.e., replacing all quoted values with their HTML entity
+   * encoded versions. (For example, `Hello"` becomes `Hello&quot;`.)
    *
    * @param s A string to be encoded.
-   * @returns A string that is HTML attribute encoded by replacing &, <, >, " and ' with their HTML entity named equivalents.
+   * @returns A string that is HTML attribute encoded by replacing `&`, `<`,
+   *   `>`, `"`, and `'` with their HTML-entity named equivalents.
    */
   public static encodeHTMLAttribute(s: string): string {
     const attributeMap: { [key: string]: string } = {
@@ -153,12 +170,15 @@ export default class Convert {
   }
 
   /**
-   * Public: Convert an Atom File Event as received from atom.project.onDidChangeFiles and convert it into an Array of
-   * Language Server Protocol {FileEvent} objects. Normally this will be a 1-to-1 but renames will be represented by a
-   * deletion and a subsequent creation as LSP does not know about renames.
+   * Public: Convert an Atom File Event as received from
+   * `atom.project.onDidChangeFiles` into an array of Language Server Protocol
+   * {@link FileEvent} objects. Normally this will be a 1-to-1 but renames will
+   * be represented by a deletion and a subsequent creation as LSP does not
+   * know about renames.
    *
-   * @param fileEvent An {atom$ProjectFileEvent} to be converted.
-   * @returns An array of LSP {ls.FileEvent} objects that equivalent conversions to the fileEvent parameter.
+   * @param fileEvent An {a@link tom$ProjectFileEvent} to be converted.
+   * @returns An array of LSP {@link ls.FileEvent} objects with equivalent
+   *   conversions to the `fileEvent` parameter.
    */
   public static atomFileEventToLSFileEvents(fileEvent: FilesystemChange): ls.FileEvent[] {
     switch (fileEvent.action) {
@@ -196,21 +216,25 @@ export default class Convert {
   }
 
   /**
-   * Public: Convert an array of language server protocol {atomIde.TextEdit} objects to an equivalent array of Atom
-   * {atomIde.TextEdit} objects.
+   * Public: Convert an array of language server protocol
+   * {@link atomIde.TextEdit} objects to an equivalent array of Atom
+   * {@link atomIde.TextEdit} objects.
    *
-   * @param textEdits The language server protocol {atomIde.TextEdit} objects to convert.
-   * @returns An {Array} of Atom {atomIde.TextEdit} objects.
+   * @param textEdits The language server protocol {@link atomIde.TextEdit}
+   *   objects to convert.
+   * @returns An array of Atom {@link atomIde.TextEdit} objects.
    */
   public static convertLsTextEdits(textEdits?: ls.TextEdit[] | null): atomIde.TextEdit[] {
     return (textEdits || []).map(Convert.convertLsTextEdit)
   }
 
   /**
-   * Public: Convert a language server protocol {atomIde.TextEdit} object to the Atom equivalent {atomIde.TextEdit}.
+   * Public: Convert a language server protocol {@link atomIde.TextEdit} object
+   * to the Atom equivalent {@link atomIde.TextEdit}.
    *
-   * @param textEdits The language server protocol {atomIde.TextEdit} objects to convert.
-   * @returns An Atom {atomIde.TextEdit} object.
+   * @param textEdit The language server protocol {@link atomIde.TextEdit}
+   *   object to convert.
+   * @returns An Atom {@link atomIde.TextEdit} object.
    */
   public static convertLsTextEdit(textEdit: ls.TextEdit): atomIde.TextEdit {
     // TODO: support annotations
